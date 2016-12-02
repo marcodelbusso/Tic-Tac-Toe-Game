ac3b6964c5440f0af0e9d01d8b1eaee40620f239
marcodelbusso <delbussm@uni.coventry.ac.uk>
2016-12-02 09:11:26 +0000

    Created a Function for the Window Properties

--- TicTacToe.py
+++ TicTacToe.py
@@ -5,10 +5,6 @@ from time import sleep
 bclick = [True] #Value used to change between 'X' and 'O'. X is true, O is false.
 
 master = Tk()
-master.resizable(0,0) #Makes the window non-resizable
-frame = Frame(master, width=3, height=576, bg="", colormap="new")
-frame.pack()
-master.title("Tic Tac Toe") #Sets the title for the window
 
 xCurrentScore = StringVar() #Label has to be updated with a textvariable, so score must be a string variable
 oCurrentScore = StringVar() #Label has to be updated with a textvariable, so score must be a string variable
@@ -16,16 +12,55 @@ oCurrentScore = StringVar() #Label has to be updated with a textvariable, so sco
 xCurrentScoreList = [0] #Stores X score value in an array so it can be updated from within a function
 oCurrentScoreList = [0] #Stores O score value in an array so it can be updated from within a function
 
-LabelX = Label(textvariable = xCurrentScore,font = ('Arial 30')) #Displays the current score for X in the GUI
-LabelX.pack(side = LEFT, padx = 10) #Formatting
 
-LabelO = Label(textvariable = oCurrentScore,font = ('Arial 30')) #Displays the current score for O in the GUI
-LabelO.pack(side = RIGHT, padx = 20) #Formatting
+def mainBoard():
+    master.title("Tic Tac Toe") #Sets the title for the window
+    master.resizable(0,0) #Makes the window non-resizable
 
-xCurrentScore.set("X's Score:   " + str(xCurrentScoreList[0])) #Tells LabelX what to display in the GUI 
-oCurrentScore.set("O's Score:   " + str(oCurrentScoreList[0])) #Tells LabelO what to display in the GUI
+    frame = Frame(master, width=3, height=576, bg="", colormap="new")
+    frame.pack()
 
-buttonsList = [" "," "," "," "," "," "," "," "," "] #Holds the
+    LabelX = Label(textvariable = xCurrentScore,font = ('Arial 30')) #Displays the current score for X in the GUI
+    LabelX.pack(side = LEFT, padx = 10) #Formatting
+
+    LabelO = Label(textvariable = oCurrentScore,font = ('Arial 30')) #Displays the current score for O in the GUI
+    LabelO.pack(side = RIGHT, padx = 20) #Formatting
+
+    xCurrentScore.set("X's Score:   " + str(xCurrentScoreList[0])) #Tells LabelX what to display in the GUI 
+    oCurrentScore.set("O's Score:   " + str(oCurrentScoreList[0])) #Tells LabelO what to display in the GUI
+
+    #Defining each button to be generated with tkinter
+
+    button1 = Button(frame, text = " ", font = ('Arial 30 bold'), height = 4, width = 8, command = lambda: tictactoe(button1,0))
+    button1.grid(row = 1, column = 0)
+
+    button2 = Button(frame, text = " ", font = ('Arial 30 bold'), height = 4, width = 8, command = lambda: tictactoe(button2,1))
+    button2.grid(row = 1, column = 1)
+
+    button3 = Button(frame, text = " ", font = ('Arial 30 bold'), height = 4, width = 8, command = lambda: tictactoe(button3,2))
+    button3.grid(row = 1, column = 2)
+
+    button4 = Button(frame, text = " ", font = ('Arial 30 bold'), height = 4, width = 8, command = lambda: tictactoe(button4,3))
+    button4.grid(row = 2, column = 0)
+
+    button5 = Button(frame, text = " ", font = ('Arial 30 bold'), height = 4, width = 8, command = lambda: tictactoe(button5,4))
+    button5.grid(row = 2, column = 1)
+
+    button6 = Button(frame, text = " ", font = ('Arial 30 bold'), height = 4, width = 8, command = lambda: tictactoe(button6,5))
+    button6.grid(row = 2, column = 2)
+
+    button7 = Button(frame, text = " ", font = ('Arial 30 bold'), height = 4, width = 8, command = lambda: tictactoe(button7,6))
+    button7.grid(row = 3, column = 0)
+
+    button8 = Button(frame, text = " ", font = ('Arial 30 bold'), height = 4, width = 8, command = lambda: tictactoe(button8,7))
+    button8.grid(row = 3, column = 1)
+
+    button9 = Button(frame, text = " ", font = ('Arial 30 bold'), height = 4, width = 8, command = lambda: tictactoe(button9,8))
+    button9.grid(row = 3, column = 2)
+
+    master.geometry('605x710+660+180')
+
+buttonsList = [" "," "," "," "," "," "," "," "," "] #Holds the text value of each button to allow comparisons for game win checks
 
 def computerOrAI():
     modeSelection = Toplevel()
@@ -159,37 +194,7 @@ def clearBoard(): #Resets the board to a default state in the list and the actua
     button8["text"] = " "
     button9["text"] = " "
 
-#Defining each button to be generated with tkinter
-
-button1 = Button(frame, text = " ", font = ('Arial 30 bold'), height = 4, width = 8, command = lambda: tictactoe(button1,0))
-button1.grid(row = 1, column = 0)
-
-button2 = Button(frame, text = " ", font = ('Arial 30 bold'), height = 4, width = 8, command = lambda: tictactoe(button2,1))
-button2.grid(row = 1, column = 1)
-
-button3 = Button(frame, text = " ", font = ('Arial 30 bold'), height = 4, width = 8, command = lambda: tictactoe(button3,2))
-button3.grid(row = 1, column = 2)
-
-button4 = Button(frame, text = " ", font = ('Arial 30 bold'), height = 4, width = 8, command = lambda: tictactoe(button4,3))
-button4.grid(row = 2, column = 0)
-
-button5 = Button(frame, text = " ", font = ('Arial 30 bold'), height = 4, width = 8, command = lambda: tictactoe(button5,4))
-button5.grid(row = 2, column = 1)
-
-button6 = Button(frame, text = " ", font = ('Arial 30 bold'), height = 4, width = 8, command = lambda: tictactoe(button6,5))
-button6.grid(row = 2, column = 2)
-
-button7 = Button(frame, text = " ", font = ('Arial 30 bold'), height = 4, width = 8, command = lambda: tictactoe(button7,6))
-button7.grid(row = 3, column = 0)
-
-button8 = Button(frame, text = " ", font = ('Arial 30 bold'), height = 4, width = 8, command = lambda: tictactoe(button8,7))
-button8.grid(row = 3, column = 1)
-
-button9 = Button(frame, text = " ", font = ('Arial 30 bold'), height = 4, width = 8, command = lambda: tictactoe(button9,8))
-button9.grid(row = 3, column = 2)
-
-master.geometry('605x710+660+180')
-
+mainBoard()
 computerOrAI()
 
 master.protocol("WM_DELETE_WINDOW", on_closing) #Generates an event for when the user presses the close button on the window
